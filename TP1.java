5bced95e39fc93e45a79067c499b7754bceb061c
Kairo Viana <116967676+Kyro42@users.noreply.github.com>
2026-08-17 20:38:39 -0300

    arquivos iniciais para o trabalho

---
+++ TP1.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.lang.NumberFormatException;
+
+public class TP1 {
+    public static void main(String[] args) {
+        
+    }
+}
