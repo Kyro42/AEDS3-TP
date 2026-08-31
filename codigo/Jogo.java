d9af0b1f44b42033a538e139ebdada034741b7ee
Kairo Viana <116967676+Kyro42@users.noreply.github.com>
2026-08-31 17:22:06 -0300

    menu + carregamento da base de dados funcionando

---
+++ codigo/Jogo.java
@@ -0,0 +1,115 @@
+import java.text.DecimalFormat;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Jogo{
+    protected int game_id;
+    protected String game_name;
+    protected LocalDate game_release;
+    protected float game_price;
+    protected String[] game_genres;
+    protected String game_description;
+
+    //construtores
+    public Jogo(int id, String name, String release, float price, String genres, String description) {
+        this.game_id = id;
+        this.game_name = name;
+
+        //transforma a data de lançamento que vem como String para LocalDate
+        //se na data de lançamento tiver algo como "Coming soon" ou vier incompleta, define a data como 01/01/1970
+        this.game_release = LocalDate.of(1970, 1, 1);
+        
+        if (release != null && !release.trim().isEmpty()) {
+            
+            //caso a data esteja completa, faz a atribuição normalmente
+            try{
+                String[] split = release.split("[\\-]");
+                int ano = Integer.parseInt(split[0]);
+                int mes = Integer.parseInt(split[1]);
+                int dia = Integer.parseInt(split[2]);
+                this.game_release = LocalDate.of(ano, Month.of(mes), dia);
+            }catch(Exception e){
+
+            }
+        }
+
+        this.game_price = price;
+        String[] generos = genres.split(", ");
+        this.game_genres = generos.clone();
+        this.game_description = description;
+    }
+
+    public Jogo() {
+        this.game_id = -1;
+        this.game_name = "";
+
+        int ano = 1970;
+        int mes = 1;
+        int dia = 1;
+        this.game_release = LocalDate.of(ano, Month.of(mes), dia);
+
+        this.game_price = 0;
+        this.game_genres = new String[] { "" };
+        this.game_description = "";
+    }
+
+    public byte[] toByteArray() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dados = new DataOutputStream(baos);
+        
+        dados.writeInt(this.game_id);
+        
+        // String fixa (100 caracteres)
+        // Preenche com espaços vazios à direita e passar de 100 para de inserir
+        String nome = String.format("%-100s", this.game_name);
+        nome = nome.substring(0, 100);
+        dados.writeUTF(nome);
+        
+        // Grava como um long 
+        dados.writeLong(this.game_release.toEpochDay());
+        
+        dados.writeFloat(this.game_price);
+        
+        // Junta o array de strings colocando o "-"
+        String generos = String.join("-", this.game_genres);
+        dados.writeUTF(generos);
+        
+        dados.writeUTF(this.game_description);
+        return baos.toByteArray();
+    }
+
+    public void fromByteArray(byte[] ba) throws IOException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(ba);
+        DataInputStream dis = new DataInputStream(bais);
+
+        this.game_id = dis.readInt();
+
+        // Lê a string fixa de 100 caracteres e usa trim() para tirar os espaços extras
+        this.game_name = dis.readUTF().trim();
+
+        // Converte a data do long
+        this.game_release = LocalDate.ofEpochDay(dis.readLong());
+
+        this.game_price = dis.readFloat();
+
+        // Lê a string de gêneros e quebra ela de volta para um array usando o "-"
+        String stringGeneros = dis.readUTF();
+        this.game_genres = stringGeneros.split("\\-");
+
+        this.game_description = dis.readUTF();
+    }
+
+    public String toString() {
+        DecimalFormat preco = new DecimalFormat("#,##0.00");
+        String result = "\nID: " + game_id + "\nNome: " + game_name + "\nData de lançamento: " + game_release
+                + "\nPreço: USD" + preco.format(game_price) + "\nGeneros: " + String.join(", ", game_genres) + "\nDescrição: " + game_description;
+        
+        return result;
+    }
+}
--- codigo/TP1.java
+++ codigo/TP1.java
@@ -1,34 +1,116 @@
+import java.util.List;
+import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.lang.NumberFormatException;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 public class TP1 {
     public static void main(String[] args) {
-        FileOutputStream arq; //arquivo de saída
-        DataOutputStream flux; //fluxo de saida de dados
-        FileInputStream arq2; //arquivo de entrada
-        DataInputStream flux2; //fluxo de entrada de dados
-        try {
-            arq = new FileOutputStream("../dataBase/jogos.db");
-            flux = new DataOutputStream(arq); //conecta o fluxo de dados com o arquivo
-
-            File file = new File("../dataBase/steam_games.csv");
-            if (!file.exists()) {
-                System.out.println("CSV não encontrado");
+        Scanner sc = new Scanner(System.in);
+        criaMenu();
+        int opt = sc.nextInt();
+        while (opt != 0) {
+            switch (opt) {
+                case 1:
+                    leitorCSV();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Numero invalido!");
             }
-            
-            RandomAccessFile dataBase = new RandomAccessFile(file, "r"); //acessa o arquivo csv com modo leitura
+            criaMenu();
+            opt = sc.nextInt();
+        }
+        sc.close();
+    }
 
-        } catch(IOException e){
-            System.out.println("Erro: " + e);
+    public static void criaMenu() {
+        String titulo = "\n-----------Steam Games DB----------";
+        String barra = "-----------------------------------\n";
+        String opcoes = String.format("\n%s\n%s\n%s\n%s\n%s", "[1] Carregar base de dados", "[2] Ler registro",
+                "[3] Atualizar registro", "[4] Deletar registro", "[0] Sair");
+
+        System.out.println(titulo);
+        System.out.println("Selecione:");
+        System.out.println(opcoes);
+        System.out.println(barra);
+    }
+
+    public static void leitorCSV() {
+        Path caminhoCSV = Paths.get("../dataBase/steam_games.csv");
+        String caminhoBinario = "../database/jogos.db";
+        int ultimoId = 0;
+
+        try (BufferedReader leitor = Files.newBufferedReader(caminhoCSV)) {
+            RandomAccessFile arq = new RandomAccessFile(caminhoBinario, "rw");
+            String linha;
+            leitor.readLine();
+            while ((linha = leitor.readLine()) != null) {
+                String[] valores = linha.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"); //valores[4] = generos
+                if (valores.length == 6) {
+
+                    int id = Integer.parseInt(valores[0]);
+                    String nome = valores[1];
+                    String lancamento = valores[2];
+                    float preco = 0.0f;
+                    if(!valores[3].trim().isEmpty()){
+                        try{
+                            preco = Float.parseFloat(valores[3]);
+                        } catch(Exception e){
+
+                        }
+                    }
+
+                    String generosRaw = valores[4]; // os generos do jogo estão assim: "[""genero1"",""genero2""]". Quero deixa-los assim: genero1, genero2.
+
+                    String generos = generosRaw.replace("[", "").replace("]", "").replace("\"", "");
+                    valores[4] = generos;
+
+                    String descricao = valores[5];
+
+                    if (id > ultimoId) {
+                        ultimoId = id;
+                    }
+
+                    Jogo temp = new Jogo(id, nome, lancamento, preco, generos, descricao);
+                    byte[] ba;
+
+                    try {
+                        ba = temp.toByteArray();
+                        arq.writeByte(0); // Byte da lápide: 0 = valido, 1 = excluido
+                        arq.writeInt(ba.length);
+                        arq.write(ba);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                }
+            }
+            arq.seek(0);
+            arq.writeInt(ultimoId);
+            System.out.println("\nBase de dados carregada com sucesso! Ultimo ID: " + ultimoId);
+        } catch (IOException e) {
+            System.out.println(e);
         }
     }
+
 }
